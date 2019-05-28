d05b51b4f10897d7cbac8b952c79de426389c589
TheProjectM <themonthlyproject@gmail.com>
2019-05-28 15:47:28 +0800

    add caffe.sh and mini-caffe.sh

---
+++ Shell/scripts/caffe.sh
@@ -0,0 +1,4 @@
+cd ~
+git clone https://github.com/TheProjectM/caffe.git
+cd caffe
+sh ./build.sh
---
+++ Shell/scripts/mini-caffe.sh
@@ -0,0 +1,5 @@
+sudo apt install libopenblas-dev libprotobuf-dev protobuf-compiler
+cd ~
+git clone https://github.com/TheProjectM/mini-caffe.git
+cd mini-caffe
+sh ./build.sh
